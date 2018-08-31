e41ef39b56992afbd256a5c726aba4e3a4dc72d2
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2018-08-31 20:15:59 +0300

    register bundle in kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -49,6 +49,7 @@ class AppKernel extends Kernel
             new PrestaShop\TranslationToolsBundle\TranslationToolsBundle(),
             // Api consumer
             new Csa\Bundle\GuzzleBundle\CsaGuzzleBundle(),
+            new League\Tactician\Bundle\TacticianBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
