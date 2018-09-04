0ce62f64262605189572ce7262c0caf61c512d71
sadlyblue <sadb55@gmail.com>
2018-09-04 16:41:10 +0100

    Check minimal quantity
    Solve #10162. After the product is added to the cart, it will return an error if the customer tries to decrease the product quantity below the minimal_quantity (for the product or combination).

--- controllers/front/CartController.php
+++ controllers/front/CartController.php
@@ -333,6 +333,35 @@ class CartControllerCore extends FrontController
         } elseif (!$product->checkQty($qty_to_check)) {
             $this->errors[] = $this->trans('There are not enough products in stock', array(), 'Shop.Notifications.Error');
         }
+        
+        // Check minimal_quantity
+        if(!$this->id_product_attribute){
+            if($qty_to_check < $product->minimal_quantity){
+                 array_push(
+                     $this->{$ErrorKey},
+                     $this->trans(
+                         'The minimum purchase order quantity for the product %product% is %quantity%.',
+                         array('%product%' => $product->name, '%quantity%' => $product->minimal_quantity),
+                         'Shop.Notifications.Error'
+                     )
+                 );
+                 return;
+            }
+        }
+        else{
+             $combination = new Combination($this->id_product_attribute);
+             if($qty_to_check < $combination->minimal_quantity){
+                 array_push(
+                     $this->{$ErrorKey},
+                     $this->trans(
+                         'The minimum purchase order quantity for the product %product% is %quantity%.',
+                         array('%product%' => $product->name, '%quantity%' => $combination->minimal_quantity),
+                         'Shop.Notifications.Error'
+                     )
+                 );
+                 return;
+            }
+        }
 
         // If no errors, process product addition
         if (!$this->errors) {
