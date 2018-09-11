5e63257eda9260b49add3439a1f0cfdc506d9370
Mreker <mathias@reker.dk>
2018-09-11 11:43:38 +0200

    Property Address->id_country is empty

--- classes/Address.php
+++ classes/Address.php
@@ -214,6 +214,10 @@ class AddressCore extends ObjectModel
             Customer::resetAddressCache($this->id_customer, $this->id);
         }
 
+        if (empty($this->id)) {
+            return true;
+        }
+        
         if (!$this->isUsed()) {
             return parent::delete();
         } else {
