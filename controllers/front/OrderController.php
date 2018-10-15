9823d2747154a2b879f9e219cf04e16201545aac
Thomas LEVIANDIER <thomas.leviandier@gmail.com>
2018-10-15 20:50:47 +0200

    refresh checkout page when on payment step if cart is modified

--- controllers/front/OrderController.php
+++ controllers/front/OrderController.php
@@ -244,6 +244,10 @@ class OrderControllerCore extends FrontController
             'cart' => $presentedCart,
         ]);
 
+        $this->context->smarty->assign([
+            'display_transaction_updated_info' => Tools::getIsset('updatedTransaction'),
+        ]);
+
         parent::initContent();
         $this->setTemplate('checkout/checkout');
     }
